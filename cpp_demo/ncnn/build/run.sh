5afc882f6a1e9a36cd7a9f2b06aa862652a0031b
xmk <xmk@mac.com>
2022-09-17 12:14:01 +0800

    编译ncnn的时候先不要安装opencv, 先编译完ncnn再安装opencv

--- cpp_demo/ncnn/build/run.sh
+++ cpp_demo/ncnn/build/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-YOLO_PATH=/Volumes/outDisk/git/IoT/YOLOv5-Lite
+#YOLO_PATH=/Volumes/outDisk/git/IoT/YOLOv5-Lite
+YOLO_PATH=/Users/xmk/git/IoT/YOLOv5-Lite
 ./ncnnv5lite $YOLO_PATH/cpp_demo/ncnn/images/horse.jpg
