b816d9f52cc13471928a310fa41def0fcd66a292
xmk <xmk@mac.com>
2022-09-19 23:31:17 +0800

    异常

--- cpp_demo/ncnn/v5lite-cgo.cpp
+++ cpp_demo/ncnn/v5lite-cgo.cpp
@@ -1,32 +1,47 @@
 
 #include "v5lite-s.h"
-
-extern "C" {
-   #include "v5lite-cgo.h"
+#include <iostream>
+using namespace std;
+extern "C"
+{
+#include "v5lite-cgo.h"
 }
 
-int test() {
+int test()
+{
     printf("hello v5lite-cgo");
     return 0;
 }
-typedef struct cnnex {
+typedef struct cnnex
+{
     ncnn::Extractor *ex;
     uint8_t data;
 } cnnex;
 
-cnnex* newCnnex() {
-    cnnex *c = (cnnex*)malloc(sizeof(cnnex)); //创建一个结构体含有N个数据
+cnnex *newCnnex()
+{
+    cnnex *c = (cnnex *)malloc(sizeof(cnnex)); //创建一个结构体含有N个数据
     c->ex = initYolov5();
     c->data = 8;
     return c;
 }
 
-int runDetect(cnnex *c, byte *bytes, int width, int height) {
+int runDetect(cnnex *c, byte *bytes, int width, int height)
+{
     printf(
-        "runDetect c->ex:%p c->data:%d bytes:%s width:%d height:%d", 
-        c->ex, c->data, ((char *)bytes), width, height
-    );
-    // return detectByYolov5(*(c->ex), bytes, width, height);
-    c->ex->set_num_threads(2);
-    return 0;
+        "runDetect c->ex:%p c->data:%d bytes:%s width:%d height:%d",
+        c->ex, c->data, ((char *)bytes), width, height);
+    try
+    {
+        throw exception();
+        // return detectByYolov5(*(c->ex), bytes, width, height);
+        c->ex->set_num_threads(2);
+        return 0;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "runDetect caught" << std::endl;
+        std::cout << e.what() << std::endl;
+        return -1;
+    }
 }
