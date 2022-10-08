dda103f168c7607831323f318cd4fca07a4a64e5
interface <627795061@qq.com>
2022-10-08 12:01:57 +0800

    编译通过

--- cpp_demo/ncnn/build-aarch64/build.sh
+++ cpp_demo/ncnn/build-aarch64/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 THREAD=6
-cmake -DTARGET_CPU:STRING=aarch64 -DCMAKE_TOOLCHAIN_FILE=../toolchains/aarch64-linux-gnu.toolchain.cmake .. --build . -j $THREAD
+cmake -DTARGET_CPU:STRING=aarch64 -DCMAKE_TOOLCHAIN_FILE=../toolchains/aarch64-linux-gnu.toolchain.cmake .. 
+cake --build . -j $THREAD
 cmake --build . --target install
--- cpp_demo/ncnn/build/build.sh
+++ cpp_demo/ncnn/build/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 THREAD=6
-cmake -DTARGET_CPU:STRING=x86 --build . -j $THREAD
+cmake -DTARGET_CPU:STRING=x86 ..
+cmake --build . -j $THREAD
 cmake --build . --target install
