866d39f7a48ff509e849db24fbab1424e85a3a78
interface <627795061@qq.com>
2022-10-08 15:14:26 +0800

    段错误调试

--- cpp_demo/ncnn/build/run.sh
+++ cpp_demo/ncnn/build/run.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 #YOLO_PATH=/Volumes/outDisk/git/IoT/YOLOv5-Lite
-YOLO_PATH=/Users/xmk/git/IoT/YOLOv5-Lite
+
+HOST=`whoami`
+if [ "$HOST" == "interface" ]
+then
+    echo "[interface]"
+    YOLO_PATH=/Volumes/outDisk/git/IoT/YOLOv5-Lite
+elif [ "$HOST" == "xmk" ]
+then
+    echo "[xmk]"
+    YOLO_PATH=/Users/${HOST}/git/IoT/YOLOv5-Lite
+else
+    echo "[pi]"
+fi
+
+
 ./ncnnv5lite $YOLO_PATH/cpp_demo/ncnn/images/horse.jpg
--- cpp_demo/ncnn/v5lite-s.cpp
+++ cpp_demo/ncnn/v5lite-s.cpp
@@ -522,19 +522,181 @@ int detectByYolov5(ncnn::Extractor& ex, byte *bytes, int width, int height) {
         w = w * scale;
     }
 
-    ncnn::Mat in = ncnn::Mat::from_pixels_resize(bgr.data, ncnn::Mat::PIXEL_BGR2RGB, img_w, img_h, w, h);
+    ncnn::Mat in = ncnn::Mat::from_pixels_resize(
+        bgr.data, ncnn::Mat::PIXEL_BGR2RGB, img_w, img_h, w, h
+    );
 
     // pad to target_size rectangle
     // yolov5/utils/datasets.py letterbox
     int wpad = (w + 31) / 32 * 32 - w;
     int hpad = (h + 31) / 32 * 32 - h;
     ncnn::Mat in_pad;
-    ncnn::copy_make_border(in, in_pad, hpad / 2, hpad - hpad / 2, wpad / 2, wpad - wpad / 2, ncnn::BORDER_CONSTANT, 114.f);
+    ncnn::copy_make_border(
+        in, in_pad, 
+        hpad / 2, 
+        hpad - hpad / 2, 
+        wpad / 2, 
+        wpad - wpad / 2, 
+        ncnn::BORDER_CONSTANT, 114.f
+    );
 
     const float norm_vals[3] = {1 / 255.f, 1 / 255.f, 1 / 255.f};
     in_pad.substract_mean_normalize(0, norm_vals);
-
     ex.set_num_threads(4);
+
+    // 这里会报错，如果图片数据不对
+    ex.input("images", in_pad);
+
+    std::vector<Object> proposals;
+
+    // stride 8
+    {
+        ncnn::Mat out;
+        ex.extract("output", out);
+
+        ncnn::Mat anchors(6);
+        anchors[0] = 10.f;
+        anchors[1] = 13.f;
+        anchors[2] = 16.f;
+        anchors[3] = 30.f;
+        anchors[4] = 33.f;
+        anchors[5] = 23.f;
+
+        std::vector<Object> objects8;
+        generate_proposals(anchors, 8, in_pad, out, prob_threshold, objects8);
+
+        proposals.insert(proposals.end(), objects8.begin(), objects8.end());
+    }
+    // stride 16
+    {
+        ncnn::Mat out;
+        ex.extract("1111", out);
+
+        ncnn::Mat anchors(6);
+        anchors[0] = 30.f;
+        anchors[1] = 61.f;
+        anchors[2] = 62.f;
+        anchors[3] = 45.f;
+        anchors[4] = 59.f;
+        anchors[5] = 119.f;
+
+        std::vector<Object> objects16;
+        generate_proposals(anchors, 16, in_pad, out, prob_threshold, objects16);
+
+        proposals.insert(proposals.end(), objects16.begin(), objects16.end());
+    }
+    // stride 32
+    {
+        ncnn::Mat out;
+        ex.extract("2222", out);
+
+        ncnn::Mat anchors(6);
+        anchors[0] = 116.f;
+        anchors[1] = 90.f;
+        anchors[2] = 156.f;
+        anchors[3] = 198.f;
+        anchors[4] = 373.f;
+        anchors[5] = 326.f;
+
+        std::vector<Object> objects32;
+        generate_proposals(anchors, 32, in_pad, out, prob_threshold, objects32);
+
+        proposals.insert(proposals.end(), objects32.begin(), objects32.end());
+    }
+
+    // sort all proposals by score from highest to lowest
+    qsort_descent_inplace(proposals);
+
+    // apply nms with nms_threshold
+    std::vector<int> picked;
+    nms_sorted_bboxes(proposals, picked, nms_threshold);
+
+    int count = picked.size();
+
+    objects.resize(count);
+    for (int i = 0; i < count; i++)
+    {
+        objects[i] = proposals[picked[i]];
+
+        // adjust offset to original unpadded
+        float x0 = (objects[i].rect.x - (wpad / 2)) / scale;
+        float y0 = (objects[i].rect.y - (hpad / 2)) / scale;
+        float x1 = (objects[i].rect.x + objects[i].rect.width - (wpad / 2)) / scale;
+        float y1 = (objects[i].rect.y + objects[i].rect.height - (hpad / 2)) / scale;
+
+        // clip
+        x0 = std::max(std::min(x0, (float)(img_w - 1)), 0.f);
+        y0 = std::max(std::min(y0, (float)(img_h - 1)), 0.f);
+        x1 = std::max(std::min(x1, (float)(img_w - 1)), 0.f);
+        y1 = std::max(std::min(y1, (float)(img_h - 1)), 0.f);
+
+        objects[i].rect.x = x0;
+        objects[i].rect.y = y0;
+        objects[i].rect.width = x1 - x0;
+        objects[i].rect.height = y1 - y0;
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    time = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec);
+    printf(">> detect Time : %lf ms\n", (double)time/1000000);
+    draw_objects(bgr, objects);
+    return 0;
+}
+
+int testMe(ncnn::Extractor& ex, const cv::Mat bgr) {
+    std::vector<Object> objects;
+    // const cv::Mat bgr = bytesToMat(bytes, width, height);
+    struct timespec begin, end;
+    long time;
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+
+    const int target_size = 320;
+    const float prob_threshold = 0.60f;
+    const float nms_threshold = 0.60f;
+
+    int img_w = bgr.cols;
+    int img_h = bgr.rows;
+
+    // letterbox pad to multiple of 32
+    int w = img_w;
+    int h = img_h;
+    float scale = 1.f;
+    if (w > h)
+    {
+        scale = (float)target_size / w;
+        w = target_size;
+        h = h * scale;
+    }
+    else
+    {
+        scale = (float)target_size / h;
+        h = target_size;
+        w = w * scale;
+    }
+
+    ncnn::Mat in = ncnn::Mat::from_pixels_resize(
+        bgr.data, ncnn::Mat::PIXEL_BGR2RGB, img_w, img_h, w, h
+    );
+
+    // pad to target_size rectangle
+    // yolov5/utils/datasets.py letterbox
+    int wpad = (w + 31) / 32 * 32 - w;
+    int hpad = (h + 31) / 32 * 32 - h;
+    ncnn::Mat in_pad;
+    ncnn::copy_make_border(
+        in, in_pad, 
+        hpad / 2, 
+        hpad - hpad / 2, 
+        wpad / 2, 
+        wpad - wpad / 2, 
+        ncnn::BORDER_CONSTANT, 114.f
+    );
+
+    const float norm_vals[3] = {1 / 255.f, 1 / 255.f, 1 / 255.f};
+    in_pad.substract_mean_normalize(0, norm_vals);
+    ex.set_num_threads(4);
+
+    // 这里会报错，如果图片数据不对
     ex.input("images", in_pad);
 
     std::vector<Object> proposals;
@@ -675,15 +837,17 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    std::vector<Object> objects;
-    detect_yolov5(m, objects);
+    // std::vector<Object> objects;
+    // detect_yolov5(m, objects);
 
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    time = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec);
-    printf(">> Time : %lf ms\n", (double)time/1000000);
+    // clock_gettime(CLOCK_MONOTONIC, &end);
+    // time = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec);
+    // printf(">> Time : %lf ms\n", (double)time/1000000);
 
-    draw_objects(m, objects);
+    // draw_objects(m, objects);
 
+    ncnn::Extractor* ex = initYolov5();
+    testMe(*ex, m);
     return 0;
 }
 #endif
